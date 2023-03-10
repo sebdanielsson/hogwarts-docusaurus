a6214a3758b36883fcfbdcff629618acb4b739d4
Sebastian <sebastian.danielsson@proton.me>
2023-03-10 19:02:56 +0100

    Add html head tag for Mastodon account link verification

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,5 +1,14 @@
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
+  headTags: [
+    {
+      tagName: "link",
+      attributes: {
+        rel: "me",
+        href: "https://mastodon.social/@sebbo",
+      },
+    },
+  ],
   themes: [
     [
       require.resolve("@easyops-cn/docusaurus-search-local"),
@@ -17,78 +26,80 @@ module.exports = {
         highlightSearchTermsOnTargetPage: true,
         searchResultLimits: 8,
         searchResultContextMaxLength: 50,
-        explicitSearchResultPath: false
+        explicitSearchResultPath: false,
       },
     ],
   ],
-  title: 'Hogwarts',
-  tagline: 'Guides for macOS & Linux',
-  url: 'https://hogwarts.zone',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/hogwarts-icon-dark.webp',
-  organizationName: 'SebDanielsson',
-  projectName: 'hogwarts-docusaurus',
+  title: "Hogwarts",
+  tagline: "Guides for macOS & Linux",
+  url: "https://hogwarts.zone",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/hogwarts-icon-dark.webp",
+  organizationName: "SebDanielsson",
+  projectName: "hogwarts-docusaurus",
   themeConfig: {
     colorMode: {
-      defaultMode: 'dark',
+      defaultMode: "dark",
       respectPrefersColorScheme: true,
     },
     prism: {
-      theme: require('prism-react-renderer/themes/dracula'),
-      additionalLanguages: ['nginx','systemd'],
+      theme: require("prism-react-renderer/themes/dracula"),
+      additionalLanguages: ["nginx", "systemd"],
     },
     navbar: {
-      title: 'Hogwarts',
+      title: "Hogwarts",
       logo: {
-        alt: 'Hogwarts Logo',
-        src: 'img/hogwarts-icon-dark.webp',
+        alt: "Hogwarts Logo",
+        src: "img/hogwarts-icon-dark.webp",
       },
       items: [
-        {to: '/', label: 'Blog', position: 'left'},
-        {to: '/contact', label: 'Contact', position: 'left'},
+        { to: "/", label: "Blog", position: "left" },
+        { to: "/contact", label: "Contact", position: "left" },
         //{to: '/landingpage', label: 'Landingpage', position: 'left'},
         //{
-          //type: 'doc',
-          //docId: 'intro',
-          //position: 'left',
-          //label: 'Docs',
+        //type: 'doc',
+        //docId: 'intro',
+        //position: 'left',
+        //label: 'Docs',
         //},
         {
-          href: 'https://github.com/sebdanielsson',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/sebdanielsson",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/SebDanielsson/hogwarts-docusaurus/edit/main/docs/',
+            "https://github.com/SebDanielsson/hogwarts-docusaurus/edit/main/docs/",
         },
         blog: {
-          routeBasePath: '/',
+          routeBasePath: "/",
           showReadingTime: false,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/SebDanielsson/hogwarts-docusaurus/edit/main/',
+            "https://github.com/SebDanielsson/hogwarts-docusaurus/edit/main/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
   ],
   scripts: [
     {
-      src: 'https://plausible.hogwarts.zone/js/script.js', defer: true, 'data-domain': 'hogwarts.zone'
-    }
+      src: "https://plausible.hogwarts.zone/js/script.js",
+      defer: true,
+      "data-domain": "hogwarts.zone",
+    },
   ],
 };
