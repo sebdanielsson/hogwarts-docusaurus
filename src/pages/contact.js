a62c502e581d1e577117847f43d7a876e17e5439
Sebastian <sebastian.danielsson@proton.me>
2023-03-11 17:32:20 +0100

    Remove unused css import

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,32 +1,31 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
-import ContactForm from '../components/ContactForm';
-import { useForm, ValidationError } from '@formspree/react';
+import React from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import ContactForm from "../components/ContactForm";
+import { useForm, ValidationError } from "@formspree/react";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext(), [state, handleSubmit] = useForm("mdozlgpe");
-  
+  const { siteConfig } = useDocusaurusContext(),
+    [state, handleSubmit] = useForm("mdozlgpe");
+
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">Get in touch!</h1>
-        <p className="hero__subtitle">Fill out the form below to send me an email.</p>
+        <p className="hero__subtitle">
+          Fill out the form below to send me an email.
+        </p>
       </div>
     </header>
   );
-
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={`Contact`}
-      description="Contact hogwarts.zone">
+    <Layout title={`Contact`} description="Contact hogwarts.zone">
       <HomepageHeader />
       <main>
         <ContactForm />
