0cece7313eddd5733dc0f273d11a52d1412349b3
Sebastian <sebastian.danielsson@proton.me>
2023-06-16 12:56:57 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,100 +1,81 @@
-/** @type {import('@docusaurus/types').DocusaurusConfig} */
-module.exports = {
-  /* themes: [
-    [
-      require.resolve("@easyops-cn/docusaurus-search-local"),
-      {
-        hashed: true,
-        language: ["en"],
-        indexDocs: true,
-        indexBlog: true,
-        indexPages: true,
-        docsRouteBasePath: "/docs",
-        blogRouteBasePath: "/",
-        docsDir: "docs",
-        blogDir: "blog",
-        removeDefaultStopWordFilter: false,
-        highlightSearchTermsOnTargetPage: true,
-        searchResultLimits: 8,
-        searchResultContextMaxLength: 50,
-        explicitSearchResultPath: false,
+/** @returns {Promise<import('@docusaurus/types').Config>} */
+module.exports = async function createConfigAsync() {
+  return {
+    title: "Hogwarts",
+    tagline: "Guides for macOS & Linux",
+    url: "https://hogwarts.zone",
+    baseUrl: "/",
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
+    favicon: "img/hogwarts-icon-dark.webp",
+    organizationName: "SebDanielsson",
+    projectName: "hogwarts-docusaurus",
+    themeConfig: {
+      colorMode: {
+        defaultMode: "dark",
+        respectPrefersColorScheme: true,
       },
-    ],
-  ], */
-  title: "Hogwarts",
-  tagline: "Guides for macOS & Linux",
-  url: "https://hogwarts.zone",
-  baseUrl: "/",
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/hogwarts-icon-dark.webp",
-  organizationName: "SebDanielsson",
-  projectName: "hogwarts-docusaurus",
-  themeConfig: {
-    colorMode: {
-      defaultMode: "dark",
-      respectPrefersColorScheme: true,
-    },
-    prism: {
-      theme: require("prism-react-renderer").themes.dracula,
-      additionalLanguages: ["nginx", "systemd"],
-    },
-    navbar: {
-      title: "Hogwarts",
-      logo: {
-        alt: "Hogwarts Logo",
-        src: "img/hogwarts-icon-dark.webp",
+      prism: {
+        theme: require("prism-react-renderer").themes.dracula,
+        additionalLanguages: ["nginx", "systemd"],
       },
-      items: [
-        //{ to: "/", label: "Blog", position: "left" },
-        //{to: '/landingpage', label: 'Landingpage', position: 'left'},
-        //{
-        //type: 'doc',
-        //docId: 'intro',
-        //position: 'left',
-        //label: 'Docs',
-        //},
-        {
-          href: "https://github.com/sebdanielsson",
-          label: "GitHub",
-          position: "right",
+      navbar: {
+        title: "Hogwarts",
+        logo: {
+          alt: "Hogwarts Logo",
+          src: "img/hogwarts-icon-dark.webp",
         },
+        items: [
+          //{ to: "/", label: "Blog", position: "left" },
+          //{to: '/landingpage', label: 'Landingpage', position: 'left'},
+          //{
+          //type: 'doc',
+          //docId: 'intro',
+          //position: 'left',
+          //label: 'Docs',
+          //},
+          {
+            href: "https://github.com/sebdanielsson",
+            label: "GitHub",
+            position: "right",
+          },
+          {
+            type: "html",
+            position: "right",
+            value:
+              '<a target="_blank" href="https://mastodon.social/@sebbo" rel="me" class="navbar__link">Mastodon</a>',
+          },
+        ],
+      },
+    },
+    presets: [
+      [
+        "@docusaurus/preset-classic",
         {
-          type: "html",
-          position: "right",
-          value:
-            '<a target="_blank" href="https://mastodon.social/@sebbo" rel="me" class="navbar__link">Mastodon</a>',
+          docs: {
+            sidebarPath: require.resolve("./sidebars.js"),
+            editUrl:
+              "https://github.com/SebDanielsson/hogwarts-docusaurus/edit/main/docs/",
+          },
+          blog: {
+            routeBasePath: "/",
+            showReadingTime: false,
+            // Please change this to your repo.
+            editUrl:
+              "https://github.com/SebDanielsson/hogwarts-docusaurus/edit/main/",
+          },
+          theme: {
+            customCss: require.resolve("./src/css/custom.css"),
+          },
         },
       ],
-    },
-  },
-  presets: [
-    [
-      "@docusaurus/preset-classic",
+    ],
+    scripts: [
       {
-        docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
-          editUrl:
-            "https://github.com/SebDanielsson/hogwarts-docusaurus/edit/main/docs/",
-        },
-        blog: {
-          routeBasePath: "/",
-          showReadingTime: false,
-          // Please change this to your repo.
-          editUrl:
-            "https://github.com/SebDanielsson/hogwarts-docusaurus/edit/main/",
-        },
-        theme: {
-          customCss: require.resolve("./src/css/custom.css"),
-        },
+        src: "https://plausible.hogwarts.zone/js/script.js",
+        defer: true,
+        "data-domain": "hogwarts.zone",
       },
     ],
-  ],
-  scripts: [
-    {
-      src: "https://plausible.hogwarts.zone/js/script.js",
-      defer: true,
-      "data-domain": "hogwarts.zone",
-    },
-  ],
+  };
 };
