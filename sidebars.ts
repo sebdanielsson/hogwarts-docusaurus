a675caa82e3453f0c795c3c72668d7ab6bf3047e
Sebastian <sebastian.danielsson@proton.me>
2023-11-09 11:27:55 +0100

    Fix invalid path

--- sidebars.ts
+++ sidebars.ts
@@ -1,7 +1,7 @@
 import type {SidebarsConfig} from '@docusaurus/plugin-content-docs';
 
 const sidebars: SidebarsConfig = {
-  docs: ['introduction'],
+  docs: ['intro'],
 };
 
 export default sidebars;
